0267e3037294cbf3e59c20484e85d4263fc71d4b
udcw1995 <udcwickramarathna@gmail.com>
2023-03-12 16:33:29 +0530

    InitialAuthor routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@
 const express = require('express')
 const app = express()
 const expresslayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expresslayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://udcw:udcw@mybrary.6vftonh.mongodb.net/?retryWrites=true&w=majority', {
@@ -22,5 +25,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
